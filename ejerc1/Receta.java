07676e88ca0841917b8959736c9a02e0f06add9d
Jose Antonio Núñez <joseextremo@gmail.com>
2017-05-16 07:11:52 +0200

    Ahora he hecho los dos constructores. Uno vacio y otro con los tres atributos.
    Signed-off-by: Jose Antonio Núñez <joseextremo@gmail.com>

--- ejerc1/Receta.java
+++ ejerc1/Receta.java
@@ -1,5 +1,19 @@
 package ejerc1;
 
-public class Receta {
+public class Receta 
+{
+	private String nombre;
+	private String ingredientes;
+	private String preparacion;
+	
+	public Receta(String nombre) {
+		this.nombre = nombre;
+	}
 
+	public Receta(String nombre, String ingredientes, String preparacion) {
+		this.nombre = nombre;
+		this.ingredientes = ingredientes;
+		this.preparacion = preparacion;
+	}
+	
 }
