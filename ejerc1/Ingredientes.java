5b718a028bdfef5feca4178ee5b785f86ed775f2
Jose Antonio Núñez <joseextremo@gmail.com>
2017-05-16 07:24:47 +0200

    Creo la clase Ingrdientes con sus respectivos atributos.
    Signed-off-by: Jose Antonio Núñez <joseextremo@gmail.com>

---
+++ ejerc1/Ingredientes.java
@@ -0,0 +1,9 @@
+package ejerc1;
+
+public class Ingredientes 
+{
+	private String cantidad;
+	private String nombre;
+	
+	
+}
--- ejerc1/Receta.java
+++ ejerc1/Receta.java
@@ -40,5 +40,4 @@ public class Receta
 		this.preparacion = preparacion;
 	}
 	
-	
 }
