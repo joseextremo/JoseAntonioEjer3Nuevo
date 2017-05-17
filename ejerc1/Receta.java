c865075a064ee17fcb7faa83f6769c98ac94bdc9
Jose Antonio Núñez <joseextremo@gmail.com>
2017-05-17 12:59:31 +0200

    He cambiado el atributo ingredientes de String por un ArrayList
    Signed-off-by: Jose Antonio Núñez <joseextremo@gmail.com>

--- ejerc1/Receta.java
+++ ejerc1/Receta.java
@@ -1,19 +1,29 @@
 package ejerc1;
 
+import java.util.ArrayList;
+
 public class Receta 
 {
 	private String nombre;
-	private String ingredientes;
+	private ArrayList<String> ingredientes = new ArrayList();
 	private String preparacion;
 	
 	public Receta(String nombre) {
 		this.nombre = nombre;
 	}
 
-	public Receta(String nombre, String ingredientes, String preparacion) {
+	public Receta(String nombre, ArrayList<String> ingredientes, String preparacion) {
 		this.nombre = nombre;
 		this.ingredientes = ingredientes;
 		this.preparacion = preparacion;
+	}
+	
+	public ArrayList<String> getIngredientes() {
+		return ingredientes;
+	}
+
+	public void setIngredientes(ArrayList<String> ingredientes) {
+		this.ingredientes = ingredientes;
 	}
 
 	public String getNombre() {
@@ -22,14 +32,6 @@ public class Receta
 
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
-	}
-
-	public String getIngredientes() {
-		return ingredientes;
-	}
-
-	public void setIngredientes(String ingredientes) {
-		this.ingredientes = ingredientes;
 	}
 
 	public String getPreparacion() {
