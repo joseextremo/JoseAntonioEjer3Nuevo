41163a83d61eb46396d1a502101505f53901d7c7
Jose Antonio Núñez <joseextremo@gmail.com>
2017-05-17 12:54:52 +0200

    He añadido un constructor vacio y otro con dos parametros y getters y setters
    Signed-off-by: Jose Antonio Núñez <joseextremo@gmail.com>

--- ejerc1/Ingredientes.java
+++ ejerc1/Ingredientes.java
@@ -5,5 +5,13 @@ public class Ingredientes
 	private String cantidad;
 	private String nombre;
 	
+	public Ingredientes(String cantidad, String nombre) {
+		this.cantidad = cantidad;
+		this.nombre = nombre;
+	}
+
+	public Ingredientes() {
+		
+	}
 	
 }
